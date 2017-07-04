fd4f310b4bfae86ce6c56612c959d2662ce334b6
WZRunfree <wangzirun93@hotmail.com>
2017-07-04 22:18:44 +0800

    commit 010

---
+++ 010.RegularExpressionMatching/Solution.h
@@ -0,0 +1,33 @@
+/**
+Implement regular expression matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "a*") → true
+isMatch("aa", ".*") → true
+isMatch("ab", ".*") → true
+isMatch("aab", "c*a*b") → true */
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if(p.empty()) return s.empty();
+        
+        if(p[1] == '*') {
+            return isMatch(s, p.substr(2)) 
+                || (!s.empty() && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p));
+        }
+        else
+            return !s.empty() && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p.substr(1));
+        return false;
+    }
+};
